c7cc95ad78a002011a579840307e59d14ea1e9c9
eob <eyob.alamerew.belay@gmail.com>
2023-03-31 10:25:04 +0300

    Manage error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const photoData = await uploadPhoto();
-  const userData = await createUser();
-  const text = `${photoData.body} ${userData.firstName} ${userData.lastName}`;
+  try {
+    const photoData = await uploadPhoto().catch();
+    const userData = await createUser();
+    const text = `${photoData.body} ${userData.firstName} ${userData.lastName}`;
+    console.log(text);
+  } catch {
+    console.log('Signup system offline');
+  }
 
-  console.log(text);
 }
